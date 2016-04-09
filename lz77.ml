339f0c8e8d1215cc4feea4c266232dc11e46bacc
Chan Ngo <chan.ngo2203@gmail.com>
2016-04-09 00:12:00 -0400

    chagne llz77

--- lz77.ml
+++ lz77.ml
@@ -15,8 +15,10 @@ let match_index = ref 0
 (* check compressed file extension *)
 let check_extension filename = 
 	let len = String.length filename in 
-	if (len < 5) then raise (Invalid_file_ext)
-	else
+	if (len < 5) then 
+		begin
+			raise (Invalid_file_ext)
+		end else
 		begin
 			let extension = String.sub filename (len - 5) 5 in 
 			let uppercaseext = String.uppercase extension in 
@@ -28,87 +30,97 @@ let check_extension filename =
 let uncompress_filename filename = 
 	let len = String.length filename in 
 	if (len < 5) then raise (Invalid_file_ext)
-	else 
-		String.sub filename (0) (len - 5)
+	else String.sub filename (0) (len - 5)
 
 (* get filename with extension *)
 let compress_filename filename = 
-	if (get_debug ()) then print_endline (filename);
 	filename ^ ".lz77"
 
 (* trim the search buffer *)
 let trim_search_buffer b = 
-	if (get_debug ()) then print_endline ("trim search buffer");
+	let _ = if (get_debug ()) then print_endline ("trim search buffer") in
+	
 	let trim_b = Buffer.create default_buff_size in
-	let trim_bytes = Bytes.create default_buff_size in Buffer.blit b ((Buffer.length b) - default_buff_size) trim_bytes 0 default_buff_size; Buffer.add_bytes trim_b trim_bytes; trim_b
+	let trim_bytes = Bytes.create default_buff_size in 
+	let _ = Buffer.blit b ((Buffer.length b) - default_buff_size) 
+		trim_bytes 0 default_buff_size in
+		Buffer.add_bytes trim_b trim_bytes; trim_b
 	 
 (* compress function 
  * 0: succeeded
  * 1: failed
  *)		 	
 let compress filename = 
+	(* debug *)
+	let _ = if (get_debug ()) then print_endline ("Text file: " ^ filename) in
+	
 	let ic = open_in filename in 
 	let chars = char_stream ic in 
 	let comfilename = compress_filename filename in 
-	if (get_debug ()) then print_endline (comfilename);
+	
+	(* debug *)
+	let _ = if (get_debug ()) then print_endline ("Compressed file: " ^ comfilename) in
+	
 	let oc = open_out comfilename in   
 	let search_buffer = Buffer.create default_buff_size in 
 	current_match := ""; match_index := 0; 
 	try
 		while true do
+			(* get the next char from text file *)
 			let next_char = Stream.next chars in 
 			try 
-			begin
 				let r = Str.regexp (!current_match ^ (String.make 1 next_char)) in 
 				(* look in our search buffer for a match *)
 				let tmp_index = Str.search_forward r (Buffer.contents search_buffer) 0 in 
 				(* if match then append next_char to current_match and update match index *)
 				current_match := !current_match ^ (String.make 1 next_char); 
 				match_index := tmp_index
-			end
+				
 			with Not_found -> 
-			begin
 				(* found the longest match *)
-				if (get_debug ()) then print_endline ("longest match: " ^ !current_match);
-				let codedstr = "~" ^ (string_of_int !match_index) ^ "~" ^ (string_of_int (String.length !current_match)) ^ "~" ^ (String.make 1 next_char) in
+				let _ = if (get_debug ()) then print_endline ("longest match: " ^ !current_match) in 
+				(* coded string of the current match *)
+				let codedstr = "~" ^ (string_of_int !match_index) ^ "~" 
+					^ (string_of_int (String.length !current_match)) ^ "~" 
+					^ (String.make 1 next_char) in
 				let concat = !current_match ^ (String.make 1 next_char) in 
 				if ((String.length codedstr) <= (String.length concat)) then 
-				begin
-					(* output codedstr *)
-					output_string oc codedstr;
-					(* append concat to search buffer *)
-					Buffer.add_string search_buffer concat;
-					(* reset current_match and match_index *)
-					current_match := "";
-					match_index := 0
-				end
-				else 
-				begin
-					(* otherwise, output chars one at a time *)
-					current_match := !current_match ^ (String.make 1 next_char); 
-					match_index := -1;
-					while ((String.length !current_match) > 1 && !match_index = -1) do
 					begin
-					output_char oc (String.get !current_match 0);
-					Buffer.add_char search_buffer (String.get !current_match 0);
-					current_match := String.sub !current_match 1 (String.length !current_match - 1);
-					try
-						let r1 = Str.regexp (!current_match) in 
-						let tmp_index1 = Str.search_forward r1 (Buffer.contents search_buffer) 0 in
-						match_index := tmp_index1
-					with Not_found ->
-						match_index := -1	
-					end
-					done
-				end
-			end;
+						(* output codedstr *)
+						output_string oc codedstr;
+						(* append concat to search buffer *)
+						Buffer.add_string search_buffer concat;
+						(* reset current_match and match_index *)
+						current_match := "";
+						match_index := 0
+					end else 
+					begin
+						(* otherwise, output chars one at a time *)
+						current_match := !current_match ^ (String.make 1 next_char); 
+						match_index := -1;
+						while ((String.length !current_match) > 1 && !match_index = -1) do
+							begin
+								output_char oc (String.get !current_match 0);
+								Buffer.add_char search_buffer (String.get !current_match 0);
+								current_match := String.sub !current_match 1 (String.length !current_match - 1);
+								try
+									let r1 = Str.regexp (!current_match) in 
+									let tmp_index1 = Str.search_forward r1 (Buffer.contents search_buffer) 0 in
+									match_index := tmp_index1
+								with Not_found ->
+									match_index := -1	
+							end
+						done
+					end;
+					
 			(* trim the search buffer *)
-			if (Buffer.length search_buffer > default_buff_size) then let trim_buffer = trim_search_buffer search_buffer in Buffer.clear search_buffer; Buffer.add_buffer search_buffer trim_buffer
-		done;
-		true
+			if (Buffer.length search_buffer > default_buff_size) then 
+				let trim_buffer = trim_search_buffer search_buffer in 
+				Buffer.clear search_buffer; 
+				Buffer.add_buffer search_buffer trim_buffer
+		done; true
 	with 
 		| Stream.Failure ->
-			begin
 			(* flush any thing in current_match *)
 			if (!match_index <> -1) then 
 				begin
@@ -118,12 +130,11 @@ let compress filename =
 				else output_string oc (!current_match)
 				end;
 			(* close any I/O channel *)
-			close_in ic; close_out oc; true
-			end
+			close_in ic; flush oc; close_out oc; true
 		| _ -> 
 			begin
 			(* close any I/O channel *)
-			close_in ic; close_out oc; false
+			close_in ic; flush oc; close_out oc; false
 			end
 	
 (* uncompress function 
